1b756b00b70f705a56a9d7c9463c1a16183a9348
joohj2005 <57057173+joohj2005@users.noreply.github.com>
2021-02-17 10:30:58 +0900

    Update onemoon-level-bot.py

--- onemoon-level-bot.py
+++ onemoon-level-bot.py
@@ -144,5 +144,5 @@ async def on_message(message):
     
     await message.channel.send(embed=embed)
 
-access_token = os.environ["BOT_TOKEN"]
+access_token = os.environ['BOT_TOKEN']
 client.run(access_token)
