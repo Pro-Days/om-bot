c7892f92a25cadaabc28e1386988d9ba9ec781b3
joohj2005 <57057173+joohj2005@users.noreply.github.com>
2021-02-19 11:08:17 +0900

    Update onemoon-level-bot.py

--- onemoon-level-bot.py
+++ onemoon-level-bot.py
@@ -37,12 +37,11 @@ async def on_message(message):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
         req = requests.get('https://skhlist.com/server/79', headers=headers)  
         html = req.text
-        soup = BeautifulSoup(html, 'html.parser')
+        soup = BeautifulSoup(html, 'html.parser', 'lxml')
 
         embed = discord.Embed(title='일월 정보', color=0x00ff56)
 
-        div = soup.find('div', {'class': 'table-responsive'})
-        tds = div.find_all('td')
+        tds = soup.find('div', {'class':'table-responsive'}).find_all('td')
 
         version = tds[0].text.strip()
         address = tds[1].text.strip()
